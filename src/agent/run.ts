56fad9b75ba88bfac7e449c03f8e8ca56e5d540a
Michelle Burton <msmichelleburton@yahoo.com>
2026-01-10 09:44:39 -0600

    added first run of Open ai agent

--- src/agent/run.ts
+++ src/agent/run.ts
@@ -1,3 +1,21 @@
 import { generateText, type ModelMessage } from 'ai'
 import { openai } from '@ai-sdk/openai'
 import { SYSTEM_PROMPT } from './system/prompt'
+import { AgentCallbacks } from '../types';
+const MODEL_NAME = "gpt-5-mini";
+
+
+export const run = async (
+    userMessage: string,
+    conversationHistory: ModelMessage[],
+    callbacks: AgentCallbacks,
+) => {
+    const { text } = await generateText({
+        model: openai(MODEL_NAME),
+        prompt: userMessage,
+        system: SYSTEM_PROMPT,
+    });
+    console.log(text)
+};
+
+run("hi my name is Michelle")
